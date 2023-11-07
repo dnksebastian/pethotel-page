03a06c40dcc98139d5ebf54e4f6c7933c95ff24a
dnksebastian <sebastian.dulniawka@gmail.com>
2023-11-07 17:22:29 +0100

    pet hotel

--- pethotel-page/src/components/Contact/Contact.jsx
+++ pethotel-page/src/components/Contact/Contact.jsx
@@ -1,10 +1,68 @@
+import { useState } from 'react';
 import './Contact.css';
 
+import { useForm } from "react-hook-form";
+
 const Contact = () => {
-    
+    const [successObj, setSuccessObj] = useState({name: '', email: '', message: ''});
+    const [showSuccess, setShowSuccess] = useState(true);
+
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        setShowSuccess(true);
+
+        const newMsgObj = {
+            name: data.cName,
+            email: data.cEmail,
+            message: data.cMessage
+        }
+
+        setSuccessObj(newMsgObj)
+
+        setTimeout(() => {
+            setShowSuccess(false)
+            setSuccessObj({name: '', email: '', message: ''})
+        }, 5000);
+
+        console.log(data)
+        reset()
+    };
+
     return (
-        <section>
-            contact
+        <section id='contact'>
+            <div className="contact-desc-wrap">
+                <h2 className="contact-heading">Contact us!</h2>
+                <p className="contact-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magnam nostrum labore voluptate ducimus rem amet dicta tempora doloremque veniam?</p>
+            </div>
+            <div className="form-wrapper">
+                <form onSubmit={handleSubmit(onSubmit)} className='contact-form'>
+                    <div className="inputs-wrap">
+                    <div className="input-helper">
+                        <label htmlFor="contact-name" className="input-label">Name:</label>
+                        <input id='contact-name' name='contact-name' type="text" {...register("cName", { required: true })} />
+                        {errors.cName && <p className="input-error">error</p>}
+                    </div>
+                    <div className="input-helper">
+                        <label htmlFor="contact-email" className="input-label">Email:</label>
+                        <input id='contact-email' name='contact-email' type="email" {...register("cEmail", { required: true })}/>
+                        {errors.cEmail && <p className="input-error">error</p>}
+                    </div>
+                    <div className="input-message-helper">
+                        <label htmlFor="contact-message" className="input-label">Message:</label>
+                        <textarea className='msg-area' name="contact-message" id="contact-message" cols="30" rows="10" placeholder='Your message...' {...register("cMessage", { required: true })}></textarea>
+                        {errors.cMessage && <p className="input-message-error">error</p>}
+                    </div>
+                    </div>
+                    <button id='contact-submit'>Send!</button>
+                </form>
+                {showSuccess && <div className="success-view">
+                    <h2 className="success-heading">Thank you{successObj.name ? `, ${successObj.name}` : ''}!</h2>
+                    <p className="success-msg">We will get back to you ass son as possible!</p>
+                    <button onClick={() => {setShowSuccess(false)}}>Ok!</button>
+                </div>}
+            </div>
         </section>
     )
 };
--- pethotel-page/src/components/FAQ/Questions.jsx
+++ pethotel-page/src/components/FAQ/Questions.jsx
@@ -1,10 +1,64 @@
 import './Questions.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
+
+const faqItems = [
+    {
+        question: 'What are your check-in and check-out times?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nemo provident, et doloremque maiores ducimus.'
+    },
+    {
+        question: "Can I bring my pet's favorite toys or bedding?",
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, veniam.'
+    },
+    {
+        question: 'What is included in the pet accommodations?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, nihil. Dolorem voluptates molestias eum voluptate laboriosam aspernatur sapiente harum culpa?'
+    },
+    {
+        question: 'Do you accommodate pets with special needs?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, fugiat.'
+    },
+    {
+        question: 'How often are pets exercised and supervised?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam laudantium quisquam quae ea dicta atque.'
+    },
+];
+
 const Questions = () => {
-    
+
+    const toggleActive = (el) => {
+        const closestFAQ = el.closest('li');
+
+        closestFAQ.classList.toggle('faq-active')
+    }
+
     return (
         <section id='questions'>
-            questions...
+            <div className="faq-desc-wrapper">
+                <h2 className="faq-heading">Frequently Asked Questions</h2>
+                <p className="faq-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos impedit ipsa culpa accusamus cum dolore quasi blanditiis ad, sequi assumenda.</p>
+            </div>
+
+            <ul className="faq-list">
+
+                {
+                faqItems.map((item, i) =>
+                <li className="faq-element" key={i}>
+                    <div className="question-helper" onClick={({ target }) => toggleActive(target)}>
+                    <p className="faq-question">{item.question}</p>
+                    <div className="faq-marker-wrap">
+                    <FontAwesomeIcon icon={faChevronUp} fixedWidth size='2x'/>
+                    </div>
+                    </div>
+                    <p className="faq-answer">{item.answer}</p>
+                </li>
+                )}
+            </ul>
+
+            <button id='faq-btn'>More Info</button>
         </section>
     )
 };
